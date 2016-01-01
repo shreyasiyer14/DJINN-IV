74e7a055f3fa42c7c20c63ac61dfb2f5cd145722
djeof-1 <shreyasiyer14@gmail.com>
2016-01-01 15:33:44 +0530

    Added an installation file for dependencies.

---
+++ PRE-INSTALL.sh
@@ -0,0 +1,4 @@
+echo "This BASH script file will install the necessary dependencies on your system (Namely Pygame, PyOpenGL, and PyOpenGL_accelerate). Please make sure that youare connected to a stable network, so that the installation doesnt halt midway. "
+
+sudo apt-get install pygame
+sudo pip install -r requirements.txt
