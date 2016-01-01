0f8f8a9e99bf39d5f2f10089ae3af62b18847bb2
djeof-1 <shreyasiyer14@gmail.com>
2016-01-01 16:20:34 +0530

    Updated Torque method.

--- djinn/physics/Torque.py
+++ djinn/physics/Torque.py
@@ -1,11 +1,13 @@
+import math
+
 class Torque:
     def __init__(self):
         self._tx = 0
         self._ty = 0
         self._tz = 0
 
-    def addTorque(self,massObj,accObj,vector,radius):
-        self._tx += massObj.mass*vector._x*vector._x*accObj._ax/radius
-        self._ty += massObj.mass*vector._y*vector._y*accObj._ay/radius
-        self._tz += massObj.mass*vector._z*vector._z*accObj._az/radius
+    def addTorque(self,massObj,accObj,radius,angle):
+        self._tx += massObj.mass*math.sin(angle)*accObj._ax*radius
+        self._ty += massObj.mass*math.sin(angle)*accObj._ay*radius
+        self._tz += massObj.mass*math.sin(angle)*accObj._az*radius
 
