e92f3102d2103a76906821ba7803f6bbb3b5590b
djeof-1 <shreyasiyer14@gmail.com>
2016-01-07 19:26:56 +0530

    Changed the ‘Box’ class entirely.

--- djinn/scenes/shapes/Box.py
+++ djinn/scenes/shapes/Box.py
@@ -1,46 +1,54 @@
 import pygame
 from pygame.locals import *
-
+from image import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
+from djinn.scenes.Light import *
+from djinn.scenes.Material import *
+from djinn.scenes.textures.Texture import *
 
 class Box:
-	def __init__(self,length,breadth,height,x,y,z):
-		self.length = length
+	def __init__(self,length,breadth,height,x,y,z,color,fname):
+		self.length = length    
 		self.breadth = breadth
 		self.height = height
 		self._x = x
 		self._y = y
 		self._z = z
-	
+		self.color = color
+		self.fname = fname
 	def build(self):
-		edges = (
-			 (0,1),
-   	 		 (0,3),
-  	 		 (0,4),
-   			 (2,1),
-   			 (2,3),
-			 (2,7),
-   		 	 (6,3),
-   			 (6,4),
-   			 (6,7),
-   			 (5,1),
-	   		 (5,4),
-   			 (5,7)
-		)
-
-		vertices = (
-			(self._x*1*self.breadth/2, -1, -1),
-   		 	(self._x*1*self.breadth/2, self._y*1*self.height, -1),
-  		  	(-1*self.breadth/2, self._y*1*self.height, -1),
-    			(-1*self.breadth/2, -1, -1),
-    			(self._x*1*self.breadth/2, -1, 1*self.length),
-    			(self._x*1*self.breadth/2, 1*self.height, 1*self.length),
-    			(-1*self.breadth/2, -1, 1*self.length),
-    			(-1*self.breadth/2, 1*self.height, 1*self.length)
-		)
-		glBegin(GL_LINES)
-    		for edge in edges:
-        		for vertex in edge:
-            			glVertex3fv(vertices[vertex])
-    		glEnd()
+                glPushMatrix()
+		material = Material()
+		material.init(128)
+                glShadeModel(GL_SMOOTH)
+		tex = Texture(self.fname)
+		glBindTexture(GL_TEXTURE_2D,tex.loadTexture())
+		glBegin(GL_QUADS)
+        	glTexCoord2f(0.0, 2.0); glVertex3f(-1.0 + self._x,  1.0*self.height + self._y,  1.0*self.length + self._z);
+    		glTexCoord2f(0.0, 0.0); glVertex3f(-1.0 + self._x, -1.0 + self._y,  1.0*self.length + self._z);
+      		glTexCoord2f(2.0, 0.0); glVertex3f( 1.0*self.breadth + self._x, -1.0 + self._y,  1.0*self.length + self._z);
+        	glTexCoord2f(2.0, 2.0); glVertex3f( 1.0*self.breadth + self._x,  1.0*self.height + self._y,  1.0*self.length + self._z);
+        	glTexCoord2f(2.0, 0.0); glVertex3f(-1.0 + self._x, -1.0 + self._y, -1.0 + self._z);
+        	glTexCoord2f(2.0, 2.0); glVertex3f(-1.0 + self._x,  1.0*self.height + self._y , -1.0 + self._z);
+        	glTexCoord2f(0.0, 2.0); glVertex3f( 1.0*self.breadth + self._x,  1.0*self.height + self._y, -1.0 + self._z);
+        	glTexCoord2f(0.0, 0.0); glVertex3f( 1.0*self.breadth +self._x, -1.0 + self._y, -1.0 + self._z);
+        	glTexCoord2f(0.0, 4.0); glVertex3f(-1.0 + self._x ,  1.0*self.height+self._y, -1.0 + self._z);
+        	glTexCoord2f(0.0, 0.0); glVertex3f(-1.0 + self._x ,  1.0*self.height+ self._y,  1.0*self.length + self._z);
+        	glTexCoord2f(4.0, 0.0); glVertex3f( 1.0*self.breadth + self._x,  1.0*self.height + self._y,  1.0*self.length + self._z);
+       		glTexCoord2f(4.0, 4.0); glVertex3f( 1.0*self.breadth + self._x ,  1.0*self.height + self._y, -1.0 + self._z);
+        	glTexCoord2f(4.0, 4.0); glVertex3f(-1.0 + self._x , -1.0 + self._y, -1.0 + self._z);
+        	glTexCoord2f(0.0, 4.0); glVertex3f( 1.0 *self.breadth + self._x, -1.0 + self._y, -1.0 + self._z);
+        	glTexCoord2f(0.0, 0.0); glVertex3f( 1.0*self.breadth + self._x, -1.0 + self._y,  1.0*self.length + self._z);
+        	glTexCoord2f(4.0, 0.0); glVertex3f(-1.0 + self._x , -1.0 + self._y,  1.0*self.length + self._z);
+        	glTexCoord2f(4.0, 0.0); glVertex3f( 1.0*self.breadth + self._x, -1.0 + self._y, -1.0 + self._z);
+        	glTexCoord2f(4.0, 4.0); glVertex3f( 1.0*self.breadth + self._x,  1.0*self.height + self._y, -1.0 + self._z);
+        	glTexCoord2f(0.0, 4.0); glVertex3f( 1.0*self.breadth + self._x,  1.0*self.height + self._y,  1.0*self.length + self._z);
+        	glTexCoord2f(0.0, 0.0); glVertex3f( 1.0*self.breadth + self._x , -1.0 + self._y,  1.0*self.length + self._z);
+        	glTexCoord2f(0.0, 0.0); glVertex3f(-1.0 + self._x , -1.0 + self._y, -1.0 + self._z);
+        	glTexCoord2f(4.0, 0.0); glVertex3f(-1.0 + self._x , -1.0 + self._y,  1.0*self.length + self._z);
+        	glTexCoord2f(4.0, 4.0); glVertex3f(-1.0 + self._x ,  1.0*self.height + self._y,  1.0*self.length + self._z);
+        	glTexCoord2f(0.0, 4.0); glVertex3f(-1.0 + self._x ,  1.0*self.height+ self._y, -1.0 + self._z);	
+		glEnd()
+		glDeleteTextures(1)
+                glPopMatrix()
