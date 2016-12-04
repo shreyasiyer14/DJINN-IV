c8ebfdf77eb4f7c4224ce86137a3491b90f08ef7
Shreyas Iyer <shreyasiyer14@gmail.com>
2016-12-04 23:28:49 +0530

    Added a base for AABB class.

--- djinn/physics/AABB.py
+++ djinn/physics/AABB.py
@@ -2,14 +2,17 @@ import math
 from RigidBody import *
 
 class AABB:
-    def __init__(self,initTransform, dimensions, tick):
+    #initTransform and dimensions are Vectors. (See math/Vector.py for more info)
+    def __init__(self,initTransform, dimensions):
         self._bx = initTransform._x
         self._by = initTransform._y
         self._bz = initTransform._z
         self._bmaxx = initTransform._x + dimensions._x
         self._bmaxy = initTransform._y + dimensions._y
         self._bmaxz = initTransform._z + dimensions._z
-
-    def setupCollider(self):
-        
-        
+        self._hasCollided = False
+    def checkWithOtherAABB(self, aabb):
+        if (self._bx <= aabb._bx):
+            if (self._bmaxx >= aabb._bx):
+                
+                
